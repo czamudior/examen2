b3ba6c1027fa1ae156c145de0afa0f1ef9606d43
Carlos <czamundior@educem.net>
2018-11-08 12:58:34 +0100

    activityCreados

--- app/src/main/java/com/demorgar/examen2intents/Activity3.java
+++ app/src/main/java/com/demorgar/examen2intents/Activity3.java
@@ -3,11 +3,11 @@ package com.demorgar.examen2intents;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class activity2 extends AppCompatActivity {
+public class Activity3 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_2);
+        setContentView(R.layout.activity_3);
     }
 }
